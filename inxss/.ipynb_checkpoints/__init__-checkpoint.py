f0876406897e5946a663bd220ee3b0cf93373220
Zhantao <zhantao@stanford.edu>
2024-11-04 13:02:24 -0800

    remove checkpoints & add license

--- inxss/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from .utils_spectrum import calc_Sqw_from_SpinW_results
-from .siren import SirenNet
-from .specnet import SpecNeuralRepr
-from .dataset import SpectrumDataset
-from .utils_qpath import linspace_2D_equidistant, plot_points, get_Sqw_func
-from .utils_steering import OnlineVariance
-from .particle import Particle
-from .psimask import PsiMask
